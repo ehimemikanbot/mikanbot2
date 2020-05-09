819c005a06719b501b74c2df24b5a93d12fbca95
ehimemikanbot <sunrise9love2is3all2blue@gmail.com>
2020-05-09 17:02:50 +0900

    4th

--- main.py
+++ main.py
@@ -26,25 +26,25 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-def make_button_template():
-    message_template = TemplateSendMessage(
-        alt_text="にゃーん",
-        template=ButtonsTemplate(
-            text="どれか選んでください",
-            title="タイトル",
-            actions=[
-                PostbackTemplateAction(
-                    label='ON',
-                    data='is_show=1'
-                ),
-                PostbackTemplateAction(
-                    label='OFF',
-                    data='is_show=0'
-                )
-            ]
-        )
-    )
-    return message_template
+# def make_button_template():
+#     message_template = TemplateSendMessage(
+#         alt_text="にゃーん",
+#         template=ButtonsTemplate(
+#             text="どれか選んでください",
+#             title="タイトル",
+#             actions=[
+#                 PostbackTemplateAction(
+#                     label='ON',
+#                     data='is_show=1'
+#                 ),
+#                 PostbackTemplateAction(
+#                     label='OFF',
+#                     data='is_show=0'
+#                 )
+#             ]
+#         )
+#     )
+#     return message_template
 
 
 @app.route("/")
@@ -70,35 +70,42 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_image_message(event):
-    messages = make_button_template()
-    line_bot_api.reply_message(
-        event.reply_token,
-        messages
-    )
+    # messages = make_button_template()
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     messages
+    # )
 # def handle_message(event):
 
-#     #オウム返しにいったんもどし
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text))
+    #オウム返しにいったんもどし
+    gettext=event.message.text
+    if gettext in ['天気','てんき']
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='明日は真夏日です'))
+    else:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
-@handler.add(PostbackEvent)
-def on_postback(event):
-    reply_token = event.reply_token
-    user_id = event.source.user_id
-    postback_msg = event.postback.data
 
-    if postback_msg == 'is_show=1':
-        line_bot_api.push_message(
-            to=user_id,
-            messages=TextSendMessage(text='is_showオプションは1だよ！')
-        )
-    elif postback_msg == 'is_show=0':
-        line_bot_api.push_message(
-            to=user_id,
-            messages=TextSendMessage(text='is_showオプションは0だよ！')
-        )
+# @handler.add(PostbackEvent)
+# def on_postback(event):
+#     reply_token = event.reply_token
+#     user_id = event.source.user_id
+#     postback_msg = event.postback.data
+
+#     if postback_msg == 'is_show=1':
+#         line_bot_api.push_message(
+#             to=user_id,
+#             messages=TextSendMessage(text='is_showオプションは1だよ！')
+#         )
+#     elif postback_msg == 'is_show=0':
+#         line_bot_api.push_message(
+#             to=user_id,
+#             messages=TextSendMessage(text='is_showオプションは0だよ！')
+#         )
 
 if __name__ == "__main__":
 #    app.run()
