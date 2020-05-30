a41cfec5258a1a04994b01126e31861e6fde5007
ehimemikanbot <sunrise9love2is3all2blue@gmail.com>
2020-05-30 13:02:33 +0900

    5th re

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -12,24 +13,32 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-#追加した部分
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+#herokuへのデプロイが成功したかどうかを確認するためのコード
 @app.route("/")
 def hello_world():
     return "hello world!"
 
+#LINE DevelopersのWebhookにURLを指定してWebhookからURLにイベントが送られるようにする
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
+    # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    # get request body as textz
+    # リクエストボディを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
+    # 署名を検証し、問題なければhandleに定義されている関数を呼ぶ
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -37,13 +46,26 @@ def callback():
 
     return 'OK'
 
-
+#以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=re_text(event.message.text))
+    )
 
+# 送信されたメッセージから返信内容を設定する
+def re_text(gettext):
+    if gettext=='1':
+        settext='1ですね。'
+    elif gettext=='2':
+        settext='2ですね。'
+    else:
+        settext='そのたですね。'
+    return settext
 
+# ポート番号の設定
 if __name__ == "__main__":
-    app.run()
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
