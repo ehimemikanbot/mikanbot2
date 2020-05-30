16f520430021a3e020e7fa313cfcb436f2d87205
ehimemikanbot <sunrise9love2is3all2blue@gmail.com>
2020-05-30 13:23:23 +0900

    6th timeline

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,QuickReplyButton, MessageAction, QuickReply,
 )
 
 app = Flask(__name__)
@@ -19,6 +19,14 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+lesson = {
+    '月曜日の時間割':'月曜日は\n1:こくご\n2:たいいく\n3:さんすう\nです。',
+    '火曜日の時間割':'火曜日は\n1~2:りかじっけん\n3:こくご\nです。',
+    '水曜日の時間割':'水曜日は\n1:さんすう\n2:おんがく\n3:せいかつ\nです。',
+    '木曜日の時間割':'木曜日は\n1:たいいく\n2:かていか\n3:りか\nです。',
+    '金曜日の時間割':'金曜日は\n1:さんすう\n2:こくご\n3:がっかつ\nです。'
+}
 
 #herokuへのデプロイが成功したかどうかを確認するためのコード
 @app.route("/")
@@ -49,12 +57,25 @@ def callback():
 #以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text=='時間割を教えて':
+        day_list=["月","火", "水", "木", "金"]
+        items=[QuickReplyButton(action=MessageAction(label=f"{day}",text=f"{day}曜日の時間割")) for day in day_list]
+        messages=TextSendMessage(text="何曜日の時間割ですか？",quick_reply=QuickReply(items=items))
+        line_bot_api.reply_message(event.reply_token,messages=messages)
+    elif event.message.text in lesson:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextMessage(text=lesson[event.message.text])
+        )
+    '''
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=re_text(event.message.text))
     )
+    '''
 
 # 送信されたメッセージから返信内容を設定する
+'''
 def re_text(gettext):
     if gettext=='1':
         settext='1ですね。'
@@ -63,6 +84,7 @@ def re_text(gettext):
     else:
         settext='そのたですね。'
     return settext
+'''
 
 # ポート番号の設定
 if __name__ == "__main__":
